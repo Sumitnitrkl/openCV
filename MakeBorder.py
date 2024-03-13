ef4ea004ea44fec64034ec7ee5dab688d1c6d043
Sumit kumar <121357559+Sumitnitrkl@users.noreply.github.com>
2024-03-13 16:00:38 +0530

    Add files via upload
    Added Grayscaling, resizing the image its rotation its translation(Translating an image means shifting it within a given frame of reference that can be along the x-axis and y-axis.)
    making outline borders in the image these all things are done using open CV.

---
+++ MakeBorder.py
@@ -0,0 +1,20 @@
+# Python program to explain cv2.copyMakeBorder() method 
+
+# importing cv2 
+import cv2 
+
+# path 
+path ='pure_white_image.png'
+
+# Reading an image in default mode 
+image = cv2.imread(path) 
+
+# Window name in which image is displayed 
+window_name = 'Image'
+
+# Using cv2.copyMakeBorder() method 
+image = cv2.copyMakeBorder(image, 10, 10, 10, 10, cv2.BORDER_CONSTANT, None, value = 0) 
+
+# Displaying the image 
+cv2.imshow(window_name, image) 
+cv2.waitKey(0)
